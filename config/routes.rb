4d890adb03d4be72c494c1070fe65a6e78e63ee9
Minh <minh@vinova.sg>
2011-07-25 14:43:23 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+
 =begin
   get "pages/home"
 
@@ -13,6 +14,8 @@ SampleApp::Application.routes.draw do
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   root              :to => 'pages#home'
+
+  match '/signup',  :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
