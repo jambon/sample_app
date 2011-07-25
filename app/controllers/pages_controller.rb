e28853d82a304b07736e6ada65b113165411511d
Minh <minh@vinova.sg>
2011-07-25 13:56:56 +0700

    Remove default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
   def home
+	@title = "This is Home"
   end
 
   def contact
+	@title = "This is Contact"
   end
 
+  def about
+	@title = "This is About"
+
+  end
+  
+  def help
+  @title = "This is Help"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 SampleApp::Application.routes.draw do
+=begin
   get "pages/home"
 
   get "pages/contact"
+  
+  get "pages/about"
+  
+  get "pages/help"
+=end
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  root              :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
